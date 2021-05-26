3676b3a4288151657845a390c30324ac6fea6148
Ivanildo Cândido <systechtecnologia@gmail.com>
2021-05-26 19:43:35 -0300

    Upload de imagens

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,26 @@ const router = express.Router();
 const homeController = require("../controllers/homeController");
 const userController = require("../controllers/userController");
 const postController = require("../controllers/postController");
+const imageMiddleware = require("../middlewares/imageMinddleware");
 
 router.get("/", homeController.index);
 router.get("/user/login", userController.login);
 
 router.get("/post/add", postController.add);
-router.post("/post/add", postController.addAction);
+router.post(
+  "/post/add",
+  imageMiddleware.upload,
+  imageMiddleware.resize,
+  postController.addAction,
+);
 
 router.get("/post/:slug/edit", postController.edit);
-router.post("/post/:slug/edit", postController.editAction);
+router.post(
+  "/post/:slug/edit",
+  imageMiddleware.upload,
+  imageMiddleware.resize,
+  postController.editAction,
+);
 
 router.get("/post/:slug", postController.view);
 
